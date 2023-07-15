7118c98006617f2ee0c5a8e3a02ed32236ecb708
Luciano T <tonetlds@gmail.com>
2023-07-14 23:32:34 -0300

    Test commit for pre-commit hook.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='dev-assistant-client',
-    version='0.1.5',
+    version='0.1.6',
     url='https://github.com/lucianotonet/dev-assistant-client',
     author='Luciano Tonet',
     author_email='tonetlds@gmail.com',
