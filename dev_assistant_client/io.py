fb01a71238f2462a5e64bbcfdbaaa962535d7526
Luciano T <tonetlds@gmail.com>
2023-07-18 03:13:12 -0300

    Updated io.py with Pygments for syntax highlighting and resolved issue4

--- dev_assistant_client/io.py
+++ dev_assistant_client/io.py
@@ -1,34 +1,34 @@
 import asyncio
-import datetime
 import json
-import sys
 import logging
-from colorama import Fore, Style
-from tabulate import tabulate
+from time import sleep
+from colorama import Back
+
 import requests
 from ably import AblyRealtime
+from pygments import highlight, lexers, formatters
+from pygments.style import Style
+from pygments.token import Keyword, Name, Comment, String, Error, Number, Operator, Generic
+from tabulate import tabulate
+
 from dev_assistant_client.auth import CONN, HEADERS
-from dev_assistant_client.utils import API_PATH, APP_URL, DEVICE_ID, TOKEN_FILE, now, print_json
 from dev_assistant_client.modules import files, git, terminal
-from time import sleep
+from dev_assistant_client.utils import API_PATH, APP_URL, DEVICE_ID, TOKEN_FILE
 
-
-MAX_RETRIES = 5  # Define a maximum number of retries
-MAX_PRINT_SIZE = 500  # Define a maximum size for printing data
-
+MAX_RETRIES = 5
 
 def print_data(data, headers):
-    """Prints the data in a table if it's not too large"""
-    if len(str(data)) > MAX_PRINT_SIZE:
-        print("Data is too large to display")
-    else:
-        print(tabulate(data, headers=headers, tablefmt="fancy_grid"), end="\n")
+    print(tabulate(data, headers=headers, tablefmt="fancy_grid"), end="\n")
 
+
+def colorize(content: str, lexer) -> str:
+    if content is not None:
+        content = highlight(content.strip(), lexer, formatters.TerminalFormatter(bg='dark'))
+
+    return content
 
 def execute_request(instruction):
-    """Executes the request from the server"""
     for _ in range(MAX_RETRIES):
-
         logging.info("Executing instruction")
         try:
             module = instruction.get('module')
@@ -37,7 +37,7 @@ def execute_request(instruction):
             operation = request.get('operation')
             args = request.get('args')
 
-            if module == 'files':
+            if module == 'files':                    
                 response_data = files.execute(operation, args)
             elif module == 'git':
                 response_data = git.execute(operation, args)
@@ -48,13 +48,11 @@ def execute_request(instruction):
             break
         except Exception as e:
             logging.error("Error", e)
-            sleep(1)  # Add a delay before retrying
+            sleep(0.3)
         else:
             return
 
-    with open(TOKEN_FILE, "r") as f:
-        token = f.readline()
-
+    token = read_token()
     HEADERS['Authorization'] = 'Bearer ' + token
 
     payload = json.dumps({
@@ -62,16 +60,12 @@ def execute_request(instruction):
     })
 
     send_response(instruction, payload)
-
-
+    
 def inform_received(instruction):
-    """Just sets the instruction as received"""
     for _ in range(MAX_RETRIES):
-        with open(TOKEN_FILE, "r") as f:
-            token = f.readline()
+        token = read_token()
         try:
-            url = 'https://' + APP_URL + API_PATH + '/devices/' + \
-                DEVICE_ID + '/io/'+instruction.get('id')
+            url = f'https://{APP_URL}{API_PATH}/devices/{DEVICE_ID}/io/{instruction.get("id")}'
             HEADERS['Authorization'] = 'Bearer ' + token
             response = requests.put(url, headers=HEADERS)
 
@@ -83,49 +77,43 @@ def inform_received(instruction):
                 break
             else:
                 logging.error("Error", response.status_code,
-                      response.json())
+                              response.json())
         except Exception as e:
             logging.error("Error", e)
-            sleep(2)  # Add a delay before retrying
+            sleep(0.5)
         else:
             return
 
-
 def send_response(instruction, response):
-    """Sends the response to the server"""
-    with open(TOKEN_FILE, "r") as f:
-        token = f.readline()
-    url = 'https://' + APP_URL + API_PATH + '/devices/' + \
-        DEVICE_ID + '/io/'+instruction.get('id')
+    token = read_token()
+    url = f'https://{APP_URL}{API_PATH}/devices/{DEVICE_ID}/io/{instruction.get("id")}'
     HEADERS['Authorization'] = 'Bearer ' + token
 
     for _ in range(MAX_RETRIES):
-
         try:
             response = requests.put(url, data=response, headers=HEADERS)
+            response.raise_for_status()
 
-            if response.status_code == 200:
-                output = response.json()
-                response = output.get('response')
-                logging.info(Fore.LIGHTGREEN_EX +
-                             "Sending response" + Style.RESET_ALL)
-                print_data([[json.dumps(response, indent=2)]], ["Response"])
-                break
-            else:
-                logging.error("Error", response.status_code)
+            output = response.json()
+            content = output.get('response', {}).get('content') or output.get('response', {}).get('message') or response.content
+            path = instruction.get('request', {}).get('args', {}).get('path')
+            lexer = lexers.get_lexer_for_filename(path) if path else lexers.JsonLexer()
+            content = colorize(content, lexer)
+
+            logging.info("Sending response")
+            print_data([[content]], ["Response"])
+            break
+        except requests.exceptions.HTTPError as e:
+            logging.error("HTTP error:", e)
+            break
         except Exception as e:
-            logging.error("Error", e)
-            sleep(2)  # Add a delay before retrying
-        else:
-            return
-
+            logging.error("Error:", e)
+            sleep(1)
 
 async def ably_connect():
-    """Connects to the Ably server and subscribes to the private channel"""
     logging.info("Initiating WebSocket connection...")
-    auth_url = 'https://' + APP_URL + API_PATH + '/ably-auth'
-    with open(TOKEN_FILE, "r") as f:
-        token = f.readline()
+    auth_url = f'https://{APP_URL}{API_PATH}/ably-auth'
+    token = read_token()
 
     try:
         HEADERS['Authorization'] = 'Bearer ' + token
@@ -136,37 +124,41 @@ async def ably_connect():
         return
 
     try:
-        token_url = 'https://rest.ably.io/keys/' + \
-            token_request['keyName'] + '/requestToken'
+        token_url = f'https://rest.ably.io/keys/{token_request["keyName"]}/requestToken'
         response = requests.post(token_url, json=token_request)
         token = response.json()['token']
         realtime = AblyRealtime(token=token)
         logging.info("WebSocket connection established")
     except Exception as e:
-        logging.error("Websocket error:", Fore.LIGHTYELLOW_EX + e + Style.RESET_ALL)
+        logging.error("Websocket error:", e)
         return
 
-    privateChannel = realtime.channels.get('private:dev-assistant-'+DEVICE_ID)
+    privateChannel = realtime.channels.get(
+        f'private:dev-assistant-{DEVICE_ID}')
     await privateChannel.subscribe(check_message)
 
     logging.info("Ready!", "Waiting for instructions...")
 
     while True:
         await asyncio.sleep(1)
-
-
+        
 def check_message(message):
-    """Checks the message received from the server"""
     try:
-        logging.info(Fore.LIGHTGREEN_EX +
-                     "Receiving instruction" + Style.RESET_ALL)
+        logging.info("Receiving instruction")
+        args = json.dumps(message.data.get('request').get('args'), indent=2)
+        path = message.data.get('request').get('args').get('path')
+        lexer = lexers.get_lexer_for_filename(path) if path else lexers.JsonLexer()
+        args = colorize(args, lexer)
         print_data(
             [[message.data.get('module'), message.data.get('request').get(
-                'operation'), json.dumps(message.data.get('request').get('args'), indent=2)]],
-            ["Module", "Operation", "Arguments"]
-        )
+                'operation'), args]],
+            ["Module", "Operation", "Arguments"])
         inform_received(message.data)
         execute_request(message.data)
     except Exception as e:
         logging.error("Error", e)
-        return
+        
+def read_token():
+    with open(TOKEN_FILE, 'r') as file:
+        token = file.read()
+    return token
--- issues/issue4.md
+++
@@ -1,3 +0,0 @@
-# Issue 4
-
-Migrar todos os prints para o log do python
