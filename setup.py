dd34d93dcf5f49772854c69e881330cc3ed2818b
Dev Assistant AI <devassistant@tonet.dev>
2023-09-09 02:52:50 -0300

    Bump version to 0.1.31 and update entry points in setup.py
    - Bumped the version from 0.1.30 to 0.1.31
    - Updated the entry points for console scripts in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='dev-assistant-client',
-    version='0.1.30',
+    version='0.1.31',
     url='https://github.com/lucianotonet/dev-assistant-client',
     author='Luciano Tonet',
     author_email='tonetlds@gmail.com',
@@ -14,9 +14,9 @@ setup(
     install_requires=required,
     entry_points={
         'console_scripts': [
-            'dev-assistant=dev_assistant_client.main:run',
-            'devassistant=dev_assistant_client.main:run',
-            'dev-assistant-client=dev_assistant_client.main:run',
+            'dev-assistant=dev-assistant-client.main:run',
+            'devassistant=dev-assistant-client.main:run',
+            'dev-assistant-client=dev-assistant-client.main:run',
         ],
     },
 )
