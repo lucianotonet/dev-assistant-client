ac8b19f99fdb9f6a2e6569efb372c3973de01219
Dev Assistant AI <devassistant@tonet.dev>
2023-09-11 17:00:58 -0300

    Refactored. v0.2
    Migrated to poetry

--- dev_assistant_client/io.py
+++ dev_assistant_client/io.py
@@ -1,196 +1,137 @@
-import asyncio
 import json
 import logging
-from time import sleep
-from colorama import Back, Fore, Style
-
 import requests
-from ably import AblyRealtime
-from pygments import highlight, lexers, formatters
-from pygments.token import (
-    Keyword,
-    Name,
-    Comment,
-    String,
-    Error,
-    Number,
-    Operator,
-    Generic,
-)
-from tabulate import tabulate
 
-from .auth import CONN, HEADERS
-from .modules import files
-from .modules import git_module
-from .modules import terminal
-from .utils import API_PATH, APP_URL, DEVICE_ID, TOKEN_FILE, now
+from time import sleep
+from colorama import Fore, Style
+from dev_assistant_client.modules.files import FilesModule
+from dev_assistant_client.modules.git import GitModule
+from dev_assistant_client.modules.terminal import TerminalModule
+from dev_assistant_client.utils import HEADERS, API_PATH, APP_URL, DEVICE_ID, now, read_token
 
-MAX_RETRIES = 2
+class IOAssistant:
+    MAX_RETRIES = 3
 
+    def __init__(self):
+        pass
 
-def colorize(content: str, lexer) -> str:
-    if content is not None:
-        content = highlight(
-            content.strip(), lexer, formatters.TerminalFormatter(bg="dark")
+    @staticmethod
+    def execute_request(instruction):
+        print(
+            now(),
+            Fore.LIGHTYELLOW_EX + "Executando tarefa ... " + Style.RESET_ALL,
+            sep="\t",
+            end="\t",
         )
 
-    return content
+        response = ""
+        module = instruction.get("module").lower()  # converter para minúsculas
+        request = instruction.get("request")
 
+        operation = request.get("operation")
+        args = request.get("args")
 
-def execute_request(instruction):
-    print(
-        now(),
-        Fore.LIGHTYELLOW_EX + "Executing task ... " + Style.RESET_ALL,
-        sep="\t",
-        end="\t",
-    )
-
-    response = ""
-    module = instruction.get("module").lower()  # convert to lowercase
-    request = instruction.get("request")
-
-    operation = request.get("operation")
-    args = request.get("args")
-
-    for _ in range(MAX_RETRIES):
-        try:
-            if module == "files":
-                response = files.execute(operation, args)
-            elif module == "git":
-                response = git_module.execute(operation, args)
-            elif module == "terminal":
-                response = terminal.execute(operation, args)
-            else:
-                response = "Invalid module or operation"
-            break
-        except Exception as e:
-            #  TODO: handle exceptions
-            # logging.error("Error", e)
-            print(Fore.LIGHTRED_EX + "ERROR:" + Style.RESET_ALL)
-            print(e)
-            sleep(0.5)
-        else:
-            return response
-    print(Fore.LIGHTGREEN_EX + "Done." + Style.RESET_ALL)
-    return response
-
-
-def set_as_read(instruction):
-    print(now(), "Setting as read ...", sep="\t", end="\t")
-
-    token = read_token()
-    url = f'https://{APP_URL}{API_PATH}/devices/{DEVICE_ID}/io/{instruction.get("id")}'
-    HEADERS["Authorization"] = "Bearer " + token
-
-    for _ in range(MAX_RETRIES):
-        try:
-            response = requests.put(url, headers=HEADERS)
-
-            if response.status_code == 200:
-                output = response.json()
-                response = output.get("response")
+        for _ in range(IOAssistant.MAX_RETRIES):
+            try:
+                if module == "files":
+                    response = FilesModule().execute(operation, args)
+                elif module == "git":
+                    response = GitModule().execute(operation, args)
+                elif module == "terminal":
+                    response = TerminalModule().execute(operation, args)
+                else:
+                    response = "Módulo ou operação inválida"
                 break
+            except Exception as e:
+                #  TODO: tratar exceções
+                # logging.error("Erro", e)
+                print(Fore.LIGHTRED_EX + "ERRO:" + Style.RESET_ALL)
+                print(e)
+                sleep(0.5)
             else:
-                logging.error("Error", response.status_code, response.json())
-        except Exception as e:
-            logging.error("Error", e)
-            sleep(1)
-        else:
-            return
-    print(Fore.LIGHTGREEN_EX + "Done." + Style.RESET_ALL)
-    return
+                return response
+        print(Fore.LIGHTGREEN_EX + "Concluído." + Style.RESET_ALL)
+        return response
 
-
-def send_response(instruction, data):
-    print(
-        now(),
-        Fore.LIGHTYELLOW_EX + "Sending response ... " + Style.RESET_ALL,
-        sep="\t",
-        end="\t",
-    )
-
-    token = read_token()
-    url = f'https://{APP_URL}{API_PATH}/devices/{DEVICE_ID}/io/{instruction.get("id")}'
-    HEADERS["Authorization"] = "Bearer " + token
-
-    for _ in range(MAX_RETRIES):
-        try:
-            response = requests.put(url, data=data, headers=HEADERS)
-            if response.status_code == 200:
-                output = response.json()
-                response = output.get("response")
-                break
-            else:
-                logging.error("Error", response.status_code, response.json())
-        except Exception as e:
-            print(Fore.LIGHTRED_EX + "Error:" + Style.RESET_ALL, e)
-            sleep(0.5)
-        else:
-            return
-    print(Fore.LIGHTGREEN_EX + "Done. " + Style.RESET_ALL)
-    return
-
-
-async def ably_connect():
-    logging.info("Initiating WebSocket connection...")
-    auth_url = f"https://{APP_URL}{API_PATH}/ably-auth"
-    token = read_token()
-
-    try:
-        HEADERS["Authorization"] = "Bearer " + token
-        response = requests.post(auth_url, headers=HEADERS)
-        token_request = response.json()
-    except Exception as e:
-        logging.error("Error", e)
-        return
-
-    try:
-        token_url = f'https://rest.ably.io/keys/{token_request["keyName"]}/requestToken'
-        response = requests.post(token_url, json=token_request)
-        token = response.json()["token"]
-        realtime = AblyRealtime(token=token)
-        logging.info("WebSocket connection established")
-    except Exception as e:
-        logging.error("Websocket error:", e)
-        return
-
-    privateChannel = realtime.channels.get(f"private:dev-assistant-{DEVICE_ID}")
-    await privateChannel.subscribe(process_message)
-
-    logging.info("Ready!", "Waiting for instructions...")
-
-    while True:
-        await asyncio.sleep(1)
-
-
-def process_message(message):
-    print(
-        now(),
-        Fore.LIGHTYELLOW_EX + "Receiving message ..." + Style.RESET_ALL,
-        message.data.get("feedback"),
-        sep="\t",
-    )
-    instruction = message.data
-
-    try:
-        set_as_read(instruction)
-    except Exception as e:
-        logging.error("Error", e)
-
-    try:
-        response_data = execute_request(instruction)
+    @staticmethod
+    def set_as_read(instruction):
+        print(now(), "Setting as read ...", sep="\t", end="\t")
 
         token = read_token()
+        url = f'{APP_URL}{API_PATH}/devices/{DEVICE_ID}/io/{instruction.get("id")}'
         HEADERS["Authorization"] = "Bearer " + token
 
-        payload = json.dumps({"response": response_data})
+        for _ in range(IOAssistant.MAX_RETRIES):
+            try:
+                response = requests.put(url, headers=HEADERS)
 
-        send_response(instruction, payload)
-    except Exception as e:
-        logging.error("Error", e)
+                if response.status_code == 200:
+                    output = response.json()
+                    response = output.get("response")
+                    break
+                else:
+                    logging.error("Error", response.status_code, response.json())
+            except Exception as e:
+                logging.error("Error", e)
+                sleep(1)
+            else:
+                return
+        print(Fore.LIGHTGREEN_EX + "Done." + Style.RESET_ALL)
+        return
 
+    @staticmethod
+    def send_response(instruction, data):
+        print(
+            now(),
+            Fore.LIGHTYELLOW_EX + "Sending response ... " + Style.RESET_ALL,
+            sep="\t",
+            end="\t",
+        )
 
-def read_token():
-    with open(TOKEN_FILE, "r") as file:
-        token = file.read()
-    return token
+        token = read_token()
+        url = f'{APP_URL}{API_PATH}/devices/{DEVICE_ID}/io/{instruction.get("id")}'
+        HEADERS["Authorization"] = "Bearer " + token
+
+        for _ in range(IOAssistant.MAX_RETRIES):
+            try:
+                response = requests.put(url, data=data, headers=HEADERS)
+                if response.status_code == 200:
+                    output = response.json()
+                    response = output.get("response")
+                    break
+                else:
+                    logging.error("Error", response.status_code, response.json())
+            except Exception as e:
+                print(Fore.LIGHTRED_EX + "Error:" + Style.RESET_ALL, e)
+                sleep(0.5)
+            else:
+                return
+        print(Fore.LIGHTGREEN_EX + "Done. " + Style.RESET_ALL)
+        return
+
+    @staticmethod
+    def process_message(message):
+        print(
+            now(),
+            Fore.LIGHTYELLOW_EX + "Receiving message ..." + Style.RESET_ALL,
+            message.data.get("feedback"),
+            sep="\t",
+        )
+        instruction = message.data
+
+        try:
+            IOAssistant.set_as_read(instruction)
+        except Exception as e:
+            logging.error("Error", e)
+
+        try:
+            response_data = IOAssistant.execute_request(instruction)
+
+            token = read_token()
+            HEADERS["Authorization"] = "Bearer " + token
+
+            payload = json.dumps({"response": response_data})
+
+            IOAssistant.send_response(instruction, payload)
+        except Exception as e:
+            logging.error("Error", e)
--- dev_assistant_client/modules/files.py
+++ dev_assistant_client/modules/files.py
@@ -1,125 +1,115 @@
-import os
-import shutil
-import unidiff
+class FilesModule:
+    import os
+    import shutil
+    import unidiff
 
+    def __init__(self):
+        pass
 
-def execute(operation, args):
-    if operation == "create":
-        return create(args.get("path"), args.get("content"))
-    elif operation == "read":
-        return read(args.get("path"))
-    elif operation == "update":
-        return update(args.get("path"), args.get("content"))
-    elif operation == "delete":
-        return delete(args.get("path"))
-    elif operation == "list":
-        return list_dir(args.get("path"))
-    elif operation == "copy":
-        return copy(args.get("source"), args.get("destination"))
-    elif operation == "move":
-        return move(args.get("source"), args.get("destination"))
-    elif operation == "rename":
-        return rename(args.get("source"), args.get("destination"))
-    elif operation == "apply_diff":
-        return apply_diff(args.get("path"), args.get("diff"))
-    else:
-        return {"error": f"Unknown operation: {operation}"}
+    def execute(self, operation, args):
+        operations = {
+            "create": self.create,
+            "read": self.read,
+            "update": self.update,
+            "delete": self.delete,
+            "list": self.list_dir,
+            "copy": self.copy,
+            "move": self.move,
+            "rename": self.rename,
+            "apply_diff": self.apply_diff
+        }
+        func = operations.get(operation)
+        if func:
+            return func(**args)
+        else:
+            return {"error": f"Unknown operation: {operation}"}
 
+    def create(self, path, content=None):
+        directory = self.os.path.dirname(path)
+        if not self.os.path.exists(directory):
+            self.os.makedirs(directory)
 
-def create(path, content=None):
-    directory = os.path.dirname(path)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+        with open(path, "w", encoding="utf-8") as file:
+            if content:
+                file.write(content)
+        return {"message": f"File created at {path}"}
 
-    with open(path, "w", encoding="utf-8") as file:
-        if content:
+    def read(self, path):
+        if not self.os.path.exists(path):
+            return {"error": f"Path does not exist: {path}"}
+
+        if self.os.path.isdir(path):
+            return self.list_dir(path)
+
+        with open(path, "r", encoding="utf-8") as file:
+            content = file.read()
+
+        return {"content": content}
+
+    def update(self, path, content):
+        if not self.os.path.exists(path):
+            return {"error": f"File does not exist: {path}"}
+
+        with open(path, "w", encoding="utf-8") as file:
             file.write(content)
-    return {"message": f"File created at {path}"}
 
+        return {"message": f"File updated at {path}"}
 
-def read(path):
-    if not os.path.exists(path):
-        return {"error": f"Path does not exist: {path}"}
+    def delete(self, path):
+        if not self.os.path.exists(path):
+            return {"error": f"File does not exist: {path}"}
 
-    if os.path.isdir(path):
-        return list_dir(path)
+        self.os.remove(path)
+        return {"message": f"File deleted at {path}"}
 
-    with open(path, "r", encoding="utf-8") as file:
-        content = file.read()
+    def list_dir(self, path):
+        if not self.os.path.exists(path):
+            return {"error": f"Directory does not exist: {path}"}
 
-    return {"content": content}
+        try:
+            files = self.os.listdir(path)
+        except Exception as e:
+            return {"error": f"Error listing directory: {e}"}
+        
+        return {"files": files}
 
+    def copy(self, source, destination):
+        if not self.os.path.exists(source):
+            return {"error": f"File does not exist: {source}"}
 
-def update(path, content):
-    if not os.path.exists(path):
-        return {"error": f"File does not exist: {path}"}
+        self.shutil.copy(source, destination)
+        return {"message": f"File copied from {source} to {destination}"}
 
-    with open(path, "w", encoding="utf-8") as file:
-        file.write(content)
+    def move(self, source, destination):
+        if not self.os.path.exists(source):
+            return {"error": f"File does not exist: {source}"}
 
-    return {"message": f"File updated at {path}"}
+        self.shutil.move(source, destination)
+        return {"message": f"File moved from {source} to {destination}"}
 
+    def rename(self, source, destination):
+        if not self.os.path.exists(source):
+            return {"error": f"File does not exist: {source}"}
 
-def delete(path):
-    if not os.path.exists(path):
-        return {"error": f"File does not exist: {path}"}
+        self.shutil.move(source, destination)
+        return {"message": f"File renamed from {source} to {destination}"}
 
-    os.remove(path)
-    return {"message": f"File deleted at {path}"}
+    def apply_diff(self, path, diff_instructions):
+        if not self.os.path.exists(path):
+            return {"error": f"File does not exist: {path}"}
 
+        with open(path, "r", encoding="utf-8") as file:
+            original_content = file.read()
 
-def list_dir(path):
-    if not os.path.exists(path):
-        return {"error": f"Directory does not exist: {path}"}
+        # Aplicar as instruções diff usando unidiff
+        patch_set = self.unidiff.PatchSet.from_string(diff_instructions)
+        patched_content = original_content
+        for patched_file in patch_set:
+            for hunk in patched_file:
+                patched_content = hunk.apply_to(patched_content)
 
-    try:
-        files = os.listdir(path)
-    except Exception as e:
-        return {"error": f"Error listing directory: {e}"}
-    
-    return {"files": files}
+        # Salvar o arquivo após aplicar o diff
+        with open(path, "w", encoding="utf-8") as file:
+            file.write(patched_content)
 
-
-def copy(source, destination):
-    if not os.path.exists(source):
-        return {"error": f"File does not exist: {source}"}
-
-    shutil.copy(source, destination)
-    return {"message": f"File copied from {source} to {destination}"}
-
-
-def move(source, destination):
-    if not os.path.exists(source):
-        return {"error": f"File does not exist: {source}"}
-
-    shutil.move(source, destination)
-    return {"message": f"File moved from {source} to {destination}"}
-
-
-def rename(source, destination):
-    if not os.path.exists(source):
-        return {"error": f"File does not exist: {source}"}
-
-    shutil.move(source, destination)
-    return {"message": f"File renamed from {source} to {destination}"}
-
-
-def apply_diff(path, diff_instructions):
-    if not os.path.exists(path):
-        return {"error": f"File does not exist: {path}"}
-
-    with open(path, "r", encoding="utf-8") as file:
-        original_content = file.read()
-
-    # Aplicar as instruções diff usando unidiff
-    patch_set = unidiff.PatchSet.from_string(diff_instructions)
-    patched_content = original_content
-    for patched_file in patch_set:
-        for hunk in patched_file:
-            patched_content = hunk.apply_to(patched_content)
-
-    # Salvar o arquivo após aplicar o diff
-    with open(path, "w", encoding="utf-8") as file:
-        file.write(patched_content)
-
-    return {"message": f"Diff applied to file at {path}"}
+        return {"message": f"Diff applied to file at {path}"}
